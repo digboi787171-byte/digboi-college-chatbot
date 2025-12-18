3374ad9ba6ada8766af6b82c75eb9c922ffd1aeb
digboi787171-byte <digboi787171@gmail.com>
2025-12-19 02:17:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = FAISS.load_local(
+    "digboi_vector_db",
+    embeddings,
+    allow_dangerous_deserialization=True
+)
