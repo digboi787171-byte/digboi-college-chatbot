dff3115bd01034f6c9cbef8b479c601c5899bbb0
digboi787171-byte <digboi787171@gmail.com>
2025-12-19 02:12:33 +0530

    Update app.py

--- digboicollege(chatbot)/app.py
+++ digboicollege(chatbot)/app.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
